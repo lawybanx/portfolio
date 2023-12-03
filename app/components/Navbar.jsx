533be3b64b67684feeb8a138c2d128dba2c24787
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-12-03 16:27:09 +0100

    refactored navigation

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,54 +2,82 @@ import Link from 'next/link';
 
 export default function Navbar({ toggleMenu, isMenuOpen }) {
   return (
-    <nav className='w-full bg-zinc-950 p-6 sticky top-0 z-auto'>
+    <nav className='bg-zinc-950 top-0 z-[999] flex w-full flex-col items-start justify-between border-b border-neutral-700 px-3 py-4 backdrop-blur md:flex-row md:items-center md:px-10 fixed'>
       <div className='flex flex-col md:flex-row justify-between items-center container mx-auto px-4 h-full'>
-        <div className='flex justify-between items-center w-full'>
-          <p className='text-white text-2xl '>Awwal Bankole</p>
+        <div className='flex justify-between w-full md:w-auto'>
+          <p className='text-white text-xl '>Awwal Bankole</p>
 
           <div
             id='menu'
             className='text-white cursor-pointer text-3xl md:hidden'
             onClick={toggleMenu}
           >
-            &#9776;
+            {isMenuOpen ? (
+              <svg
+                className='h-8 w-8'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            ) : (
+              <svg
+                className='h-8 w-8'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M4 6h16M4 12h16m-7 6h7'
+                />
+              </svg>
+            )}
           </div>
         </div>
 
-        <ul
-          className={`${
-            isMenuOpen ? 'flex' : 'hidden'
-          } flex-col md:flex-row md:space-x-4 md:ml-auto mt-4 md:mt-0`}
-        >
-          <li>
-            <Link href='#about'>
-              <p className='py-1 uppercase text-white hover:bg-stone-300 px-6 py-1;'>
-                About me
-              </p>
-            </Link>
-          </li>
-          <li>
-            <Link href='#contact'>
-              <p className='py-1 uppercase text-white hover:bg-stone-300 px-6 py-1;'>
-                Contact
-              </p>
-            </Link>
-          </li>
-          <li>
-            <Link href='#blog'>
-              <p className='py-1 uppercase text-white hover:bg-stone-300 px-6 py-1;'>
-                Blog
-              </p>
-            </Link>
-          </li>
-          <li>
-            <Link href='#projects'>
-              <p className='py-1 uppercase text-white hover:bg-stone-300 px-6 py-1;'>
-                Projects
-              </p>
-            </Link>
-          </li>
-        </ul>
+        <div className={`md:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
+          <ul className='flex flex-col md:flex-row md:space-x-4 md:ml-auto mt-4 md:mt-0 text-sm tracking-widest'>
+            <li>
+              <Link href='#about'>
+                <p className='block py-2 px-4 text-white hover:bg-stone-300 uppercase'>
+                  About Me
+                </p>
+              </Link>
+            </li>
+            <li>
+              <Link href='#contact'>
+                <p className='block py-2 px-4 text-white hover:bg-stone-300 uppercase'>
+                  Contact
+                </p>
+              </Link>
+            </li>
+            <li>
+              <Link href='#blog'>
+                <p className='block py-2 px-4 text-white hover:bg-stone-300 uppercase'>
+                  Blog
+                </p>
+              </Link>
+            </li>
+            <li>
+              <Link href='#projects'>
+                <p className='block py-2 px-4 text-white hover:bg-stone-300 uppercase'>
+                  Projects
+                </p>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-     <h1>Hello World</h1>
+    <main className='mx-auto w-full max-w-4xl px-4 mt-20'>
+      <h1>Hello World</h1>
     </main>
-  )
+  );
 }
