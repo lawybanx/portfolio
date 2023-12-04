f2748088eb2438fef6a52f387b76cb3ac553822b
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-12-04 20:05:06 +0100

    About section WIP

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,8 +15,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={poppins.className}>{children}</body>
+    <html lang='en' className='scroll-smooth'>
+      <body className={`${poppins.className} bg-stone-100`}>{children}</body>
     </html>
   );
 }
