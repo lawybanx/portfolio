9fa881c85036c37f5e6f6a20802b4b65829a8cac
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-12-05 23:00:15 +0100

    project cards WIP

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,5 +1,109 @@
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <>
+      <div className='flex flex-wrap justify-center my-10'>
+        {/* <div className='mx-auto px-5'> */}
+        <div className='max-w-xs cursor-pointer mx-4 my-4 rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md'>
+          <img
+            className='w-full rounded-lg object-cover object-center'
+            src='https://images.unsplash.com/photo-1580477371194-4593e3c7c6cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='product'
+          />
+          <div>
+            <p className='font-bold text-gray-500 my-4 px-4'>Project Name</p>
+
+            <p className='text-sm font-semibold text-gray-500 my-4 px-4'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+              at quam.
+            </p>
+
+            <div className='my-4 flex gap-2 px-4'>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* </div> */}
+        <div className='max-w-xs cursor-pointer mx-4 my-4 rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md'>
+          <img
+            className='w-full rounded-lg object-cover object-center'
+            src='https://images.unsplash.com/photo-1580477371194-4593e3c7c6cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='product'
+          />
+          <div>
+            <p className='font-bold text-gray-500 my-4 px-4'>Project Name</p>
+
+            <p className='text-sm font-semibold text-gray-500 my-4 px-4'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+              at quam.
+            </p>
+
+            <div className='my-4 flex gap-2 px-4'>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='max-w-xs cursor-pointer mx-4 my-4 rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md'>
+          <img
+            className='w-full rounded-lg object-cover object-center'
+            src='https://images.unsplash.com/photo-1580477371194-4593e3c7c6cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='product'
+          />
+          <div>
+            <p className='font-bold text-gray-500 my-4 px-4'>Project Name</p>
+
+            <p className='text-sm font-semibold text-gray-500 my-4 px-4'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+              at quam.
+            </p>
+
+            <div className='my-4 flex gap-2 px-4'>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='max-w-xs cursor-pointer mx-4 my-4 rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md'>
+          <img
+            className='w-full rounded-lg object-cover object-center'
+            src='https://images.unsplash.com/photo-1580477371194-4593e3c7c6cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='product'
+          />
+          <div>
+            <p className='font-bold text-gray-500 my-4 px-4'>Project Name</p>
+
+            <p className='text-sm font-semibold text-gray-500 my-4 px-4'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+              at quam.
+            </p>
+
+            <div className='my-4 flex gap-2 px-4'>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+              <div className='center relative inline-block select-none whitespace-nowrap rounded-lg bg-gray-800 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white'>
+                <div className='mt-px'>blue</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
