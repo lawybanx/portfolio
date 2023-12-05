178f3ff0c877c7b55a4d9a3f85fb71c235fd3a36
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-12-05 22:08:28 +0100

    init projects component

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import About from './components/About';
 import Header from './components/Header';
+import Projects from './components/Projects';
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
       <Header />
       <main className='mx-auto w-full max-w-4xl px-4 mt-16'>
         <About />
-        {/* <Projects /> */}
+        <Projects />
       </main>
     </>
   );
