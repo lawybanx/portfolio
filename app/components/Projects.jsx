6cf25dfb4dbbacbb9f5c0f9237b8d5bfa380ca51
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-12-08 17:36:08 +0100

    added repo link to project section

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -8,7 +8,17 @@ export default function Projects() {
     >
       <div className=' my-10'>
         <h2 className='md:text-6xl text-5xl'>Projects</h2>
-        <p>Below are some of the projects I&apos;ve worked on</p>
+        <p>
+          Below are some of the projects I&apos;ve worked on. View the rest on
+          my{' '}
+          <a
+            href='https://github.com/lawybanx?tab=repositories'
+            target='_blank'
+            className='cursor-pointer text-cyan-400 hover:underline'
+          >
+            Github repo.
+          </a>
+        </p>
       </div>
       <ProjectCard />
     </div>
