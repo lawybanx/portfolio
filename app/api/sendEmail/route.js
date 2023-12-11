ad7593e1a736f33f3f3863eae1f6213714ecc1b8
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-12-11 22:08:47 +0100

    added a html template for email

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -12,15 +12,51 @@ export async function POST(request) {
     },
   });
 
+  const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            padding: 20px;
+          }
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          }
+          h1 {
+            color: #333333;
+          }
+          p {
+            color: #555555;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>New Contact Form Submission</h1>
+          <p><strong>Name:</strong> ${name}</p>
+          <p><strong>Email:</strong> ${email}</p>
+          <p><strong>Subject:</strong> ${subject}</p>
+          <p><strong>Message:</strong> ${message}</p>
+        </div>
+      </body>
+      </html>
+    `;
+
   const mailOptions = {
     from: process.env.NODEMAILER_EMAIL,
     to: process.env.NODEMAILER_EMAIL,
     subject: `New Contact Form Submission - ${subject}`,
-    html: `
-            <p>Name: ${name} </p>
-            <p>Email: ${email} </p>
-            <p>Message: ${message} </p>
-            `,
+    html: htmlContent,
   };
 
   try {
