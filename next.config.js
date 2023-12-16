02d3c849e82693754644d3c561d9caa0253fddf6
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-12-16 17:26:55 +0100

    deployment bug fix

--- next.config.js
+++ next.config.js
@@ -4,16 +4,19 @@ module.exports = {
       {
         protocol: 'https',
         hostname: 'cdn.simpleicons.org',
+        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
+        port: '',
         pathname: '/**',
       },
     ],
